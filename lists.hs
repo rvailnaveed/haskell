e136d12b90c2dde42eab7c3c226d72ff7644f97a
rvailnaveed <37183938+rvailnaveed@users.noreply.github.com>
2019-05-02 22:38:21 +0000

    lists.hs completed

--- lists.hs
+++ lists.hs
@@ -1,2 +1,22 @@
--- TODO:
---     * Add list usage code examples corresponding to notes
+-- ++ concatenates two lists together (y to x)
+addTwo x y = x ++ y
+
+-- :, the cons operator. Prepends x to front of list y
+cons x y = x:y
+
+-- !! gets elem at index
+index x y = x !! y
+
+-- tail operation
+getTail x = tail x 
+
+getLength x = length x
+
+--drop y amount from list x
+dropSome x y = drop y x
+
+-- x amount of multiples of 13
+multiplesof13 x = take x [13, 26..]
+
+-- using list comprehension to generate even numbers
+evens = [x*2 | x <- [1..10]]
